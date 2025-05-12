883f7db071d2007fbab133e154f56294bd495b48
Hitesh-Sharma1021 <hitu12345sharma@gmail.com>
2025-05-13 03:30:06 +0530

    Making The Ui Design

--- UI.py
+++ UI.py
@@ -23,3 +23,10 @@ print(a)
 b=popmod.popcsv("file1 copy.csv",a,3)
 print(b)
 '''
+# Main UI Design
+
+print("\n\n"," * * * Welcome User * * * ","\n\n")
+print(" - - CSV Manipulating Application - - ",'\n\n')
+file_name=input("Enter The (Complete Name) Of Your Selected CSV File :- ")
+print("\n\nOK User Please Choose One Of The Options Below : \n(r) To Read\n(w) To Overrwrite\n(a) To Append/Add\n(p) To Remove \n")
+operation=input("Enter Your Choice Here :- ")
