e65d8f8c1088d5e091531e26d0afa3a06f654a49
Hitesh-Sharma1021 <hitu12345sharma@gmail.com>
2025-05-13 04:36:58 +0530

    Cleanup

--- UI.py
+++ UI.py
@@ -1,28 +1,3 @@
-# Read Module
-'''
-import readmod
-# readmod.readcsv("file1.csv","y")
-a=readmod.readcsv("file1 copy.csv","n")
-# readmod.readcsv("file1 copy.csv","y")
-# readmod.readcsv("file1 copy 2.csv","n")
-'''
-# Write Module
-'''
-import writemod
-writemod.writecsv("file1 copy 2.csv")
-'''
-# Append Module
-'''
-import appendmod
-appendmod.appendcsv("file1 copy.csv")
-'''
-# Pop Module
-'''
-import popmod
-print(a)
-b=popmod.popcsv("file1 copy.csv",a,3)
-print(b)
-'''
 # Main UI Design
 print("\n\n"," * * * Welcome User * * * ","\n\n")
 print(" - - CSV Manipulating Application - - ",'\n\n')
