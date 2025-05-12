c7965aff6af0f5f24d89e074882797ca965351fb
Hitesh-Sharma1021 <hitu12345sharma@gmail.com>
2025-05-13 02:47:31 +0530

    Creating an interface along with importing local libraries and using them.

--- UI.py
+++ UI.py
@@ -1,6 +1,12 @@
+# Read Module
+'''
 import readmod
-readmod.readcsv("file1.csv")
-readmod.readcsv("file1 copy.csv")
-readmod.readcsv("file1 copy 2.csv")
+readmod.readcsv("file1.csv","y")
+a=readmod.readcsv("file1.csv","n")
+# readmod.readcsv("file1 copy.csv","y")
+# readmod.readcsv("file1 copy 2.csv","n")
+'''
+# Write Module
+
 
 
--- readmod.py
+++ readmod.py
@@ -1,13 +1,15 @@
-def readcsv(file_name):
+def readcsv(file_name,ask):
     import csv
     with open(file_name,"r") as file_obj:
         data_list=['h','i','t','e','s']
         file_reader=csv.reader(file_obj)
         file_reader=list(file_reader)
-        print(file_reader)
-        print("\n")
-        for rows in file_reader:
-            print(rows)
+        if ask[0].lower()=="y":
+            print(file_reader)
+            print("\n")
+            for rows in file_reader:
+                print(rows)
+    return file_reader
         # file_reader.append(data_list)
         # print("\n")
         # print(file_reader)
