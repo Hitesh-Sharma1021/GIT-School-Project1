e2b121565c5e024ab426d1fb2ed56029a7a3492b
Hitesh-Sharma1021 <hitu12345sharma@gmail.com>
2025-05-13 03:44:38 +0530

    Template is complete now Functions are to be added.

--- UI.py
+++ UI.py
@@ -24,9 +24,32 @@ b=popmod.popcsv("file1 copy.csv",a,3)
 print(b)
 '''
 # Main UI Design
+ans="1"
+try:
+    while ans[0].lower()=="y":
+    
+        print("\n\n"," * * * Welcome User * * * ","\n\n")
+        print(" - - CSV Manipulating Application - - ",'\n\n')
+        file_name=input("Enter The (Complete Name) Of Your Selected CSV File :- ")
+        print("\n\nOK User Please Choose One Of The Options Below : \n(r) To Read\n(w) To Overrwrite\n(a) To Append/Add\n(p) To Remove \n")
+        operation=input("Enter Your Choice Here :- ")
 
-print("\n\n"," * * * Welcome User * * * ","\n\n")
-print(" - - CSV Manipulating Application - - ",'\n\n')
-file_name=input("Enter The (Complete Name) Of Your Selected CSV File :- ")
-print("\n\nOK User Please Choose One Of The Options Below : \n(r) To Read\n(w) To Overrwrite\n(a) To Append/Add\n(p) To Remove \n")
-operation=input("Enter Your Choice Here :- ")
+        if operation.lower()=="r":
+            pass
+        elif operation.lower()=="w":
+            pass 
+        elif operation.lower()=="a":
+            pass 
+        elif operation.lower()=="p":
+            pass 
+        else:
+            print("Sorry Your Input Was Inappropriate, Please Try Again !")
+        ans=input("Do You Want To Continue? (\"y\"/\"n\")")
+except Exception as e:
+    print("Oops An Error Occured!\n")
+    print(f"Error Was {e}\n")
+    print("Sorry For Inconvenience, Please Try Again!")
+else:
+    print("Something Wrong Happened!, Please Try Again.")
+finally:
+    print("Thank You User For Trying This Program :)  \n - - See You Soon! - -")
